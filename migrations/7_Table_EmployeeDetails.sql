b437a757d65e7867dfb4bebb3e235e7c5d3baa89
christofer.budianto <christofer.budianto@gdn-commerce.com>
2022-05-08 15:15:20 +0700

    alter

--- migrations/7_Table_EmployeeDetails.sql
+++ migrations/7_Table_EmployeeDetails.sql
@@ -7,6 +7,7 @@ create table employee_details(
     address varchar(255),
     constraint pk_employee_details primary key(id, organization_id),
     constraint fk_employee_detail_employee foreign key (employee_id, organization_id) references employees(id, organization_id)
+    constraint fk_employee_detail_organization foreign key(organization_id) REFERENCES organizations(id)
 ) partition by hash(organization_id);
 CREATE TABLE employee_details_0 PARTITION OF employee_details FOR VALUES WITH (modulus 3, remainder 0);
 CREATE TABLE employee_details_1 PARTITION OF employee_details FOR VALUES WITH (modulus 3, remainder 1);
